b7e035435d1195fcf35ec7e944408839a702a097
Nikita <nothanks000@gmail.com>
2016-10-05 14:41:01 +0300

    Привел змейку в движение. Дописал несколько новых методов/Инкапсуляция, наследование

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace snake
@@ -13,19 +14,35 @@ namespace snake
             Console.SetBufferSize(80, 25);
 
             //Отрисовка рамочки
-            HorrizontalLine line1 = new HorrizontalLine(0, 78, 0, '+');
-            HorrizontalLine line2 = new HorrizontalLine(0, 78, 24, '+');
-            VerticalLine line3 = new VerticalLine(0, 24, 0, '+');
-            VerticalLine line4 = new VerticalLine(0, 24, 78, '+');
-            line1.Draw();
-            line2.Draw();
-            line3.Draw();
-            line4.Draw();
+            HorrizontalLine upLine = new HorrizontalLine(0, 78, 0, '+');
+            HorrizontalLine downLine = new HorrizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
             //Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
-
+            snake.Draw();
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
         }
     }
 }
